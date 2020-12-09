3b6d0b4da2b57e504161c270462f38767d1f6fcd
古家優 <koga@tagbangers.co.jp>
2020-12-09 20:24:05 +1000

    fixed test

--- src/test/java/playground/order/web/OrderRestTests.java
+++ src/test/java/playground/order/web/OrderRestTests.java
@@ -2,16 +2,29 @@ package playground.order.web;
 
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.util.Files;
+import org.javamoney.moneta.Money;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.web.servlet.MockMvc;
+import playground.customer.Customer;
+import playground.order.*;
+import playground.product.Product;
 
 import java.io.File;
+import java.time.LocalDate;
+import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -27,6 +40,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class OrderRestTests {
 
     final MockMvc mvc;
+
+    final Orders orders;
+
+    @MockBean
+    private OrderService orderService;
+
+    @BeforeEach
+    void setup() {
+        var order = this.orders.findById(new Order.OrderId(UUID.fromString("bbb6f2f3-104b-489c-8048-ac08318a4898")))
+                .orElseThrow();
+        BDDMockito.given(orderService.createOrder(any())).willReturn(order);
+    }
 
     @Test
     void list() throws Exception {
