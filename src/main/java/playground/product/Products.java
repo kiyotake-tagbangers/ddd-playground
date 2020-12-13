ab0a77d71d6fa1cd873150f80dbdb5186285295e
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-12-13 21:51:51 +0900

    add domain ring annotation

--- src/main/java/playground/product/Products.java
+++ src/main/java/playground/product/Products.java
@@ -1,9 +1,11 @@
 package playground.product;
 
+import org.jmolecules.architecture.onion.simplified.DomainRing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author KOGA, Yu
  */
+@DomainRing
 public interface Products extends JpaRepository<Product, Product.ProductId> {
 }
