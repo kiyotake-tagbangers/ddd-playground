7ab1447a53743de68dd41c4ab3d579ac41ef3ae5
古家優 <koga@tagbangers.co.jp>
2020-12-14 13:53:35 +1000

    Added annotation

--- src/main/java/playground/inventory/InventoryService.java
+++ src/main/java/playground/inventory/InventoryService.java
@@ -1,10 +1,12 @@
 package playground.inventory;
 
+import org.jmolecules.architecture.onion.simplified.ApplicationRing;
 import playground.order.OrderCreated;
 
 /**
  * @author KOGA, Yu
  */
+@ApplicationRing
 public interface InventoryService {
 
     void updateStock(OrderCreated event);
